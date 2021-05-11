1478ee718a170ee2052191fb98efed1e15d8c6e8
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2021-05-11 16:19:01 +0700

    belajar this pada Arrow Function

--- JavascriptLanjutan-ES6/3.ArrowFunction/script.js
+++ JavascriptLanjutan-ES6/3.ArrowFunction/script.js
@@ -33,9 +33,69 @@
 // let jumlahHuruf = mahasiswa.map((nama) => nama.length);
 // console.log(jumlahHuruf);
 // mengembalikan nilainya dalam bentuk object
-let mahasiswa = ["Gilang", "Aselegedes", "Geblek"];
-let jumlahHuruf = mahasiswa.map((nama) => ({
-  nama,
-  jmlHuruf: nama.length,
-}));
-console.table(jumlahHuruf);
+// let mahasiswa = ["Gilang", "Aselegedes", "Geblek"];
+// let jumlahHuruf = mahasiswa.map((nama) => ({
+//   nama,
+//   jmlHuruf: nama.length,
+// }));
+// console.table(jumlahHuruf);
+
+// Konsep this pada Arrow Function
+// Constructor Function
+// const Mahasiswa = function () {
+//   this.nama = "Gilang";
+//   this.umur = 22;
+//   this.sayHello = function () {
+//     console.log(`Halo nama saya ${this.nama}, saya berumur ${this.umur} tahun`);
+//   };
+// };
+// const gilang = new Mahasiswa();
+
+// Arrow Function
+// const Mahasiswa = function () { // tidak semua function bisa dijadikan arrow function
+//   this.nama = "Gilang";
+//   this.umur = 22;
+//   this.sayHello =  () => { // kalo method bisa dijadikan arrow function
+//     console.log(`Halo nama saya ${this.nama}, saya berumur ${this.umur} tahun`);
+//   };
+// };
+// const gilang = new Mahasiswa();
+
+// Arrow Function pada Object Literal
+// const mhs1 = {
+//   nama: "Gilang",
+//   umur: 22,
+//   sayHello: () => {
+//     // console.log(`Halo nama saya ${this.nama}, saya berumur ${this.umur}`);
+//     console.log(this);
+//   },
+// };
+
+// Latian 1
+// const Mahasiswa = function () {
+//   this.nama = "Gilang";
+//   this.umur = 22;
+//   this.sayHello = function () {
+//     console.log(`Halo nama saya ${this.nama}, saya berumur ${this.umur}`);
+//   };
+//   setInterval(() => {
+//     console.log(this.umur++);
+//   }, 2000);
+// };
+// const gilang = new Mahasiswa();
+
+// latian 2
+const box = document.querySelector(".box");
+box.addEventListener("click", function () {
+  let satu = "size";
+  let dua = "caption";
+
+  if (this.classList.contains(satu)) {
+    [satu, dua] = [dua, satu];
+  }
+
+  this.classList.toggle(satu);
+  setTimeout(() => {
+    this.classList.toggle(dua);
+  }, 600);
+});
