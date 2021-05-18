a2c1399d523a728c38e0ae9cccd8270d5e42ce27
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2021-05-18 11:13:56 +0700

    latian Template Literals

--- JavascriptLanjutan-ES6/5.TemplateLiterals/script.js
+++ JavascriptLanjutan-ES6/5.TemplateLiterals/script.js
@@ -11,15 +11,98 @@
 // console.log(`${x % 2 == 0 ? "Genap" : "Ganjil"}`);
 
 // HTML Fragments
+// const mhs = {
+//   nama: "Gilang Ramadhan Hatno",
+//   umur: 22,
+//   npm: 10416142,
+//   email: "gilangramadhanhatno@gmail.com",
+// };
+// const el = `<div class="mhs">
+//   <h2>${mhs.nama}</h2>
+//   <span class="npm">${mhs.npm}</span>
+// </div>`;
+// console.log(el);
+
+// Latian
+// 1. HTML Fragments
+// const mhs = {
+//   nama: "Gilang Ramadhan Hatno",
+//   umur: 22,
+//   npm: 10416142,
+//   email: "gilangramadhanhatno@gmail.com",
+// };
+// const el = `<div class="mhs">
+//   <h2>${mhs.nama}</h2>
+//   <span>${mhs.npm}</span>
+// </div>`;
+// document.body.innerHTML = el;
+
+// 2. Looping
+// const mhs = [
+//   {
+//     nama: "Gilang Ramadhan Hatno",
+//     email: "gilangramadhanhatno@gmail.com",
+//   },
+//   {
+//     nama: "Aslegedes",
+//     email: "aslegedes@gmail.com",
+//   },
+//   {
+//     nama: "Geblek",
+//     email: "geblek@gmail.com",
+//   },
+// ];
+// const el = `<div class="mhs">
+//   ${mhs
+//     .map(
+//       (m) => `<ul>
+//     <li>${m.nama}</li>
+//     <li>${m.email}</li>
+//   </ul>`
+//     )
+//     .join("")}
+// </div>`;
+// document.body.innerHTML = el;
+
+// 3. Conditional - Ternary
+// const lagu = [
+//   {
+//     judul: "Tetap Dalam Jiwa",
+//     penyanyi: "Isyana Sarasvati",
+//   },
+//   {
+//     judul: "Kau Adalah",
+//     penyanyi: "Isyana Sarasvati",
+//     feat: "Rayi Putra",
+//   },
+// ];
+// const el = `<div class="lagu">
+//   ${lagu
+//     .map(
+//       (l) => `<ul>
+//     <li>${l.judul}</li>
+//     <li>${l.penyanyi} ${l.feat ? `(feat. ${l.feat})` : ``}</li>
+//   </ul>`
+//     )
+//     .join("")}
+// </div>`;
+// document.body.innerHTML = el;
+
+// 4. Nested - HTML Fragments bersarang
 const mhs = {
   nama: "Gilang Ramadhan Hatno",
-  umur: 22,
-  npm: 10416142,
-  email: "gilangramadhanhatno@gmail.com",
+  semester: 6,
+  mataKuliah: ["Pemrograman Web", "Perancangan Sistem Informasi", "Sistem Informasi Akuntansi", "Sistem Informasi Management"],
 };
+function cetakMataKuliah(matakuliah) {
+  return `<ol>
+    ${matakuliah.map((mk) => `<li>${mk}</li>`).join("")}
+  </ol>`;
+}
 const el = `<div class="mhs">
   <h2>${mhs.nama}</h2>
-  <span class="npm">${mhs.npm}</span>
+  <span class="semester">Semester : ${mhs.semester}</span>
+  <h4>Mata Kuliah :</h4>
+  ${cetakMataKuliah(mhs.mataKuliah)}
 </div>`;
-
-console.log(el);
+document.body.innerHTML = el;
